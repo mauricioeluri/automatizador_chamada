e4e33d1dd400913069a370b48f2041284896a894
Maurício El Uri <mauriciom.eluri@gmail.com>
2019-06-08 21:10:52 -0300

    melhora na qualidade do código

--- script.sh
+++ script.sh
@@ -1,49 +1,78 @@
 #!/bin/bash
-rm cookie
-rm saida.txt
-rm saidaAtividade.txt
 
-USER=161150897
-PASSWORD="#B@tatinhas"
+#INSIRA SEUS DADOS AQUI
+USER=123456
+PASSWORD="password"
 
+le_disciplina()
+{
+    echo "Selecione a disciplina para responder a chamada:"
+    echo "1 - Linux"
+    echo "2 - Ataques"
+    read DISCIPLINA 
+
+    if [ $DISCIPLINA == 1 ]
+    then
+        ID_DISCIPLINA="8655"
+    elif [ $DISCIPLINA == 2 ]
+    then
+        ID_DISCIPLINA="8656"
+    else
+        echo "[ERRO] Selecione um número entre 1 e 2."
+        le_disciplina
+    fi
+}
+
+le_disciplina
+#Pegando data atual
 DATE=`date +%Y%m%d`
+#Definindo nome da atividade
 ATIVIDADE="TESTEchamada_$DATE"
-
+#Arquivos temporários
+COOKIE=$(mktemp)
+PAGINAATIVIDADE=$(mktemp)
+PAGINAENVIOATIVIDADE=$(mktemp)
 
 echo -n "Realizando login... "
-curl -c "cookie" -X POST -d "username=$USER&password=$PASSWORD&rememberusername=1" --silent --output /dev/null https://moodle.unipampa.edu.br/moodle/login/index.php
+curl -c "$COOKIE" -X POST -d "username=$USER&password=$PASSWORD&rememberusername=1" --silent --output /dev/null https://moodle.unipampa.edu.br/moodle/login/index.php
 echo "concluído."
 
 echo -n "Acessando página do curso... "
-curl -b "cookie" --silent https://moodle.unipampa.edu.br/moodle/course/view.php?id=8655 | grep $ATIVIDADE > saida.txt
+curl -b "$COOKIE" --silent https://moodle.unipampa.edu.br/moodle/course/view.php?id=$ID_DISCIPLINA | grep $ATIVIDADE > $PAGINAATIVIDADE
 echo "concluído."
 
-
-LINK_CHAMADA="$(sed 's/^.*href="\(https[^"]*\)".*'$ATIVIDADE'/\1/;s/^\([^<]*\)<span.*$/\1/' saida.txt)&action=editsubmission"
-#BACKUP DA EXPRESSÃO REGULAR QUE FUNCIONA
-#LINK_CHAMADA="$(sed 's/^.*href="\(https[^"]*\)".*TESTEchamada_20190604/\1/;s/^\([^<]*\)<span.*$/\1/' saida.txt)"
+LINK_CHAMADA="$(sed 's/^.*href="\(https[^"]*\)".*'$ATIVIDADE'/\1/;s/^\([^<]*\)<span.*$/\1/' $PAGINAATIVIDADE)&action=editsubmission"
 
 echo -n "Acessando página da atividade... "
-curl -b "cookie" --silent $LINK_CHAMADA > saidaAtividade.txt
+curl -b "$COOKIE" --silent $LINK_CHAMADA > $PAGINAENVIOATIVIDADE
 echo "concluído."
 
+#PEGANDO PARÂMETRO ID DA ATIVIDADE
 IFS='=' read -a array_ID_ATIVIDADE <<< "${LINK_CHAMADA}"
 ID_ATIVIDADE=${array_ID_ATIVIDADE[1]::-7}
-linha_SESSKEY="$(cat saidaAtividade.txt | grep name=\"sesskey\" | xargs)"
+
+#PEGANDO PARÂMETRO SESSKEY
+linha_SESSKEY="$(cat $PAGINAENVIOATIVIDADE | grep name=\"sesskey\" | xargs)"
 IFS='=' read -a array_SESSKEY <<< "${linha_SESSKEY}"
 SESSKEY=${array_SESSKEY[6]::-1}
-linha_LASTMODIFIED="$(cat saidaAtividade.txt | grep name=\"lastmodified\" | xargs)"
+
+#PEGANDO PARÂMETRO LASTMODIFIED
+linha_LASTMODIFIED="$(cat $PAGINAENVIOATIVIDADE | grep name=\"lastmodified\" | xargs)"
 IFS='=' read -a array_LASTMODIFIED <<< "${linha_LASTMODIFIED}"
 LASTMODIFIED=${array_LASTMODIFIED[4]::-3}
-linha_USERID="$(cat saidaAtividade.txt | grep name=\"userid\" | xargs)"
+
+#PEGANDO PARÂMETRO USERID
+linha_USERID="$(cat $PAGINAENVIOATIVIDADE | grep name=\"userid\" | xargs)"
 IFS='=' read -a array_USERID <<< "${linha_USERID}"
 USERID=${array_USERID[3]::-3}
-linha_TEXTEDITORITEMID="$(cat saidaAtividade.txt | grep name=\"onlinetext_editor | tr " " "\n" | grep value | sed -n 2p | xargs)"
+
+#PEGANDO PARÂMETRO TEXTEDITORITEMID
+linha_TEXTEDITORITEMID="$(cat $PAGINAENVIOATIVIDADE | grep name=\"onlinetext_editor | tr " " "\n" | grep value | sed -n 2p | xargs)"
 IFS='=' read -a array_TEXTEDITORITEMID <<< "${linha_TEXTEDITORITEMID}"
 TEXTEDITORITEMID=${array_TEXTEDITORITEMID[1]}
 
 echo -n "Enviando chamada..."
-curl -b "cookie" -X POST -d "sesskey=$SESSKEY&id=$ID_ATIVIDADE&userid=$USERID&onlinetext_editor[itemid]=$TEXTEDITORITEMID&lastmodified=$LASTMODIFIED&action=savesubmission&_qf__mod_assign_submission_form=1&onlinetext_editor[format]=1&onlinetext_editor[text]=PRESENTE" --silent https://moodle.unipampa.edu.br/moodle/mod/assign/view.php --output /dev/null
+curl -b "$COOKIE" -X POST -d "sesskey=$SESSKEY&id=$ID_ATIVIDADE&userid=$USERID&onlinetext_editor[itemid]=$TEXTEDITORITEMID&lastmodified=$LASTMODIFIED&action=savesubmission&_qf__mod_assign_submission_form=1&onlinetext_editor[format]=1&onlinetext_editor[text]=PRESENTE" --silent https://moodle.unipampa.edu.br/moodle/mod/assign/view.php --output /dev/null
 echo "concluído."
 
 echo "SUA CHAMADA FOI ENVIADA COM SUCESSO!"
